3747d53490d8dfe91218e5e46e97f2b19d82f36d
Nisha <nisha@Nishas-MacBook-Pro.local>
2021-01-09 11:19:19 -0500

    Fetch films in Grid layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+function useFetch(url) {
+  console.log(url)
+  const [response, setResponse] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
+
+  useEffect(() => {
+      setLoading(true)
+
+      const controller = new AbortController();
+      const options = {
+        method: 'GET',
+        signal: controller.signal,
+      } 
+
+      const timeoutId = setTimeout(() => controller.abort(), 10000); //Set timeout for fetch
+
+      fetch(url, options)
+      .then(res => res.json()) 
+      .then(data => { 
+        console.log(data.results)
+        setResponse(data.results); 
+        setLoading(false); // Sets loading status to false when data loads
+      })
+      .catch(error => console.error(error, 'Timeout exceeded, unable to fetch movie data'))
+  }, [ url ])
+  return [ response, loading, hasError ]
+}
+
+export default function App() {
+  const classes = useStyles();
+  const [response, loading, hasError] = useFetch("https://swapi.dev/api/films")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+          
+          {loading ? <div>Loading...</div> : (hasError ? <div>Error occured.</div> : 
+          (response && response.length> 0 && response.map(d => 
+          <Grid item xs={12} ><Paper className={classes.paper}>{d.title}</Paper></Grid> 
+          )))}
+      </Grid> 
     </div>
   );
 }
-
-export default App;
