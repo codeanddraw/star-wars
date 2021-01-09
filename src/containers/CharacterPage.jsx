7d49a3bd31a3465b2f310d4127b48a55526b9cb1
Nisha <nisha@Nishas-MacBook-Pro.local>
2021-01-09 17:03:11 -0500

    Update text display styles in CharacterPage

--- src/containers/CharacterPage.jsx
+++ src/containers/CharacterPage.jsx
@@ -2,23 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: 'black'
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+    backgroundColor: 'black'
   },
+  text: {
+    color: '#fff',
+    textDecoration: 'none',
+    fontSize: '2em'
+
+  },
+  filmTitle: {
+    color: 'antiquewhite',
+    textDecoration: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: 0,
+    margin: 0
+  },
+  filmYear: {
+    color: 'antiquewhite',
+    textDecoration: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: 0,
+    margin: 0
+  }
 }));
 
 function useFetch(urls) {
@@ -27,42 +43,50 @@ function useFetch(urls) {
   const [hasError, setHasError] = useState(false)
 
   useEffect(() => {
-      setLoading(true)
-      const arr = urls.map(url => fetch(url))
+    setLoading(true)
+    const arr = urls.map(url => fetch(url))
 
-      Promise.all(arr).then(responses => {
-            return Promise.all(responses.map(function (response) {
-                return response.json();
-            }));
-        }).then(data => {
-            console.log(data);
-            setResponse(data)
-            setLoading(false); 
-        }).catch(error => {
-            console.log(error);
-        });
+    Promise.all(arr).then(responses => {
+      return Promise.all(responses.map(function (response) {
+        return response.json();
+      }));
+    }).then(data => {
+      console.log(data);
+      setResponse(data)
+      setLoading(false);
+    }).catch(error => {
+      console.log(error);
+      setHasError(true)
+    });
 
-  }, [ urls ])
-  return [ response, loading, hasError ]
+  }, [urls])
+  return [response, loading, hasError]
 }
 
 const CharacterPage = (props) => {
   const classes = useStyles();
-  const [response, loading, hasError] = useFetch(props.location.state.filmCharacters)
+  const charactersArray = props.location.state.filmCharacters,
+    filmTitle = props.location.state.filmTitle,
+    filmYear = props.location.state.filmYear.split('-')[0]
+
+  const [response, loading, hasError] = useFetch(charactersArray)
 
   return (
     <div className={classes.root}>
+      <h2 className={classes.filmTitle} > {filmTitle} </h2>
+      <h3 className={classes.filmYear} > {filmYear} </h3>
       <Grid container spacing={3}>
-          
-          {loading ? <div>Loading...</div> : (hasError ? <div>Error occured.</div> : 
-          (response && response.length> 0 && response.map(d => 
-          <Grid item xs={12} >
-            <Paper className={classes.paper} >
-            {d.name}
-            </Paper>
-          </Grid> 
+        {loading ? <div>Loading...</div> : (hasError ? <div>Error occured.</div> :
+          (response && response.length > 0 && response.map(d =>
+            <Grid item xs={12} >
+              <Paper className={classes.paper} >
+                <p className={classes.text}>
+                  {d.name}
+                </p>
+              </Paper>
+            </Grid>
           )))}
-      </Grid> 
+      </Grid>
     </div>
   );
 }
