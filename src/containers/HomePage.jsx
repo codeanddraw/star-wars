7f28d7cbd65f38f5708dbc4d289ddd4e96e818e3
Nisha <nisha@Nishas-MacBook-Pro.local>
2021-01-09 17:03:36 -0500

    Update HomePage styles

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -3,9 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
   Link
 } from "react-router-dom";
 
@@ -13,12 +10,20 @@ import {
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: 'black'
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+    backgroundColor: 'black'
   },
+  text: {
+    color: '#fff',
+    textDecoration: 'none',
+    fontSize: '2em',
+    cursor: 'pointer'
+
+  }
 }));
 
 function useFetch(url) {
@@ -28,60 +33,59 @@ function useFetch(url) {
   const [hasError, setHasError] = useState(false)
 
   useEffect(() => {
-      setLoading(true)
+    setLoading(true)
 
-      const controller = new AbortController();
-      const options = {
-        method: 'GET',
-        signal: controller.signal,
-      } 
+    const controller = new AbortController();
+    const options = {
+      method: 'GET',
+      signal: controller.signal,
+    }
 
-      const timeoutId = setTimeout(() => controller.abort(), 10000); //Set timeout for fetch
+    const timeoutId = setTimeout(() => controller.abort(), 10000); //Set timeout for fetch
 
-      fetch(url, options)
-      .then(res => res.json()) 
-      .then(data => { 
+    fetch(url, options)
+      .then(res => res.json())
+      .then(data => {
         console.log(data.results)
-        setResponse(data.results); 
+        setResponse(data.results);
         setLoading(false); // Sets loading status to false when data loads
       })
-      .catch(error => console.error(error, 'Timeout exceeded, unable to fetch movie data'))
-  }, [ url ])
-  return [ response, loading, hasError ]
+      .catch(error => {
+        console.error(error, 'Timeout exceeded, unable to fetch movie data')
+        setHasError(true)
+      })
+  }, [url])
+  return [response, loading, hasError]
 }
 
 export default function HomePage() {
   const classes = useStyles();
   const [response, loading, hasError] = useFetch("https://swapi.dev/api/films")
 
-  const handleClick = (e,d) => {
-    console.log('clicked',e.target,d.characters)
-    
-  }
-
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-          
-          {loading ? <div>Loading...</div> : (hasError ? <div>Error occured.</div> : 
-          (response && response.length> 0 && response.map(d => 
-          <Grid item xs={12} >
-            <Paper className={classes.paper} onClick={e => handleClick(e,d)}>
-            <Link 
-              to={{
-                pathname: `/home/${d.title}`, 
-                state: { 
-                  filmCharacters: d.characters,
-                  filmTitle: d.title,
-                  filmYear: d.year
-                }
-              }}>
-              {d.title}
-            </Link>
-            </Paper>
-          </Grid> 
+
+        {loading ? <div>Loading...</div> : (hasError ? <div>Error occured.</div> :
+          (response && response.length > 0 && response.map(d =>
+            <Grid item xs={12} >
+              {console.log(d)}
+              <Paper className={classes.paper}>
+                <Link className={classes.text}
+                  to={{
+                    pathname: `/home/${d.title}`,
+                    state: {
+                      filmCharacters: d.characters,
+                      filmTitle: d.title,
+                      filmYear: d.release_date
+                    }
+                  }}>
+                  {d.title}
+                </Link>
+              </Paper>
+            </Grid>
           )))}
-      </Grid> 
+      </Grid>
     </div>
   );
 }
