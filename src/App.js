05a0d09c8a1afaf09ba19b95a2a52e474bd6bb4e
Nisha <nisha@Nishas-MacBook-Pro.local>
2021-01-09 14:12:34 -0500

    Add router to HomePage container

--- src/App.js
+++ src/App.js
@@ -1,61 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
-function useFetch(url) {
-  console.log(url)
-  const [response, setResponse] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [hasError, setHasError] = useState(false)
-
-  useEffect(() => {
-      setLoading(true)
-
-      const controller = new AbortController();
-      const options = {
-        method: 'GET',
-        signal: controller.signal,
-      } 
-
-      const timeoutId = setTimeout(() => controller.abort(), 10000); //Set timeout for fetch
-
-      fetch(url, options)
-      .then(res => res.json()) 
-      .then(data => { 
-        console.log(data.results)
-        setResponse(data.results); 
-        setLoading(false); // Sets loading status to false when data loads
-      })
-      .catch(error => console.error(error, 'Timeout exceeded, unable to fetch movie data'))
-  }, [ url ])
-  return [ response, loading, hasError ]
-}
+import React from 'react';
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import HomePage from "./containers/HomePage"
 
 export default function App() {
-  const classes = useStyles();
-  const [response, loading, hasError] = useFetch("https://swapi.dev/api/films")
-
+ 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
+    <div>
+      <Switch>
+        <Route path="/:film">
           
-          {loading ? <div>Loading...</div> : (hasError ? <div>Error occured.</div> : 
-          (response && response.length> 0 && response.map(d => 
-          <Grid item xs={12} ><Paper className={classes.paper}>{d.title}</Paper></Grid> 
-          )))}
-      </Grid> 
+        </Route>
+
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
